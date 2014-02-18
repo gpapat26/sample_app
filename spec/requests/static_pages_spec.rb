8d0d3aeb32276817cc9a3087f6a0d39db3b6f2c1
gpapat26 <gpapat26@gmail.com>
2014-02-18 16:51:40 +0200

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,20 @@ describe "Static pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
-
-    
+     before { visit root_path }    
     
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+     # visit root_path
       expect(page).to have_content('Sample App')
     end
     
   it "should have the base title" do
-      visit '/static_pages/home'
+     # visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
     
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+     # visit root_path
       expect(page).not_to have_title('| Home')
     end
     
@@ -31,12 +30,12 @@ describe "Static pages" do
   describe "Help page" do
     
      it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
   end
@@ -45,12 +44,12 @@ describe "Static pages" do
   describe "About page" do
     
      it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
   end
@@ -58,12 +57,12 @@ describe "Static pages" do
   describe "Contact page" do
     
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
     
        it "should have the content 'Contact us instantly'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact us instantly')
     end
   end
