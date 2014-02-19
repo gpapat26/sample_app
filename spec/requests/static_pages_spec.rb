bb2a3e481e068f2462cab06233a6b02015d183c1
gpapat26 <gpapat26@gmail.com>
2014-02-19 12:03:16 +0200

    finalize tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "Static pages" do
 
@@ -67,6 +68,23 @@ describe "Static pages" do
     end
   end
     
+  describe "static pages" do
+  
+    it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+  end
     
   end
   
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "UserPages" do
+  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   subject { page }
   
@@ -9,7 +12,10 @@ describe "UserPages" do
    before { visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { should have_selector('p', text: 'Find me in') }
+   # puts "#{full_title('Sign up')}"
+     it { should have_title(full_title('Sign up')) }
+    it { should have_title("#{base_title} | Sign up")}
     end
   end
 
