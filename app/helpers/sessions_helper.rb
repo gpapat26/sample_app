13a0057653e0de7bd03a7dee526338e035d91b29
gpapat26 <gpapat26@gmail.com>
2014-02-24 18:02:20 +0200

    admins and delete fucntionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,10 @@ module SessionsHelper
     @current_user = user
   end
   
+   def current_user?(user)
+   @current_user == user
+   end
+  
    
     def current_user
     remember_token = User.encrypt(cookies[:remember_token])
@@ -27,5 +31,15 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
+  
+   
+    def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+    end
+
+    def store_location
+      session[:return_to] = request.url if request.get?
+    end
   
 end
